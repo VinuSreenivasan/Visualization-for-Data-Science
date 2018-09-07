4936228da1b5bcf78db78c6cbd2553f1c24a6c1f
Vinu Sreenivasan <vinu.sreenivasan92@gmail.com>
2018-09-07 04:08:59 -0600

    Homework2 code

--- hw2/js/Tree.js
+++ hw2/js/Tree.js
@@ -7,7 +7,11 @@ class Tree {
      * @param {json[]} json - array of json objects with name and parent fields
      */
     constructor(json) {
-
+        this.nodeList = [];
+        for (let i of json) {
+            let node = new Node(i.name, i.parent);
+            this.nodeList.push(node);
+        }
     }
 
     /**
@@ -15,28 +19,108 @@ class Tree {
      */
     buildTree() {
         // note: in this function you will assign positions and levels by making calls to assignPosition() and assignLevel()
+        let root;
+        for (let i = 0; i < this.nodeList.length; i++) {
+            let current = this.nodeList[i];
+            if (current.parentName === "root") {
+                root = current;
+            }
+            for (let j = 0; j < this.nodeList.length; j++) {
+                if (current.name === this.nodeList[j].parentName) {
+                    current.addChild(this.nodeList[j]);
+                }
+                if (current.parentName === this.nodeList[j].name) {
+                    current.parentNode = this.nodeList[j];
+                }
+            }
+        }
 
+        this.assignLevel(root, 0);
+        this.assignPosition(root, 0);
+
+        this.nodeList.forEach(function(d) {
+            let minPosition = 1000;
+            if (d.children.length != 0) {
+                d.children.forEach(function (n) {
+                    if (minPosition > n.position) {
+                        minPosition = n.position;
+                    }
+                });
+                d.position = minPosition;
+            }
+        });
     }
 
     /**
      * Recursive function that assign levels to each node
      */
     assignLevel(node, level) {
-
+        node.level = level;
+            for (let n of node.children) {
+                this.assignLevel(n, level+1);
+            }
     }
 
     /**
      * Recursive function that assign positions to each node
      */
     assignPosition(node, position) {
+        node.position = position;
 
+        for (let n of node.children) {
+            let maxPosition = -1, newPosition;
+
+            this.nodeList.forEach(function(d){
+                if (d.level === n.level) {
+                    if (d.position > maxPosition) {
+                        maxPosition = d.position;
+                    }
+                }
+            });
+
+            if (maxPosition === -1) {
+                newPosition = n.parentNode.position;
+            } else {
+                newPosition = maxPosition + 1;
+            }
+
+            this.assignPosition(n, newPosition);
+        }
     }
+
 
     /**
      * Function that renders the tree
      */
     renderTree() {
+        let div = d3.select("body");
+        div.append("svg")
+            .attr("width", 1200)
+            .attr("height", 1200);
 
+        let svg = d3.select("svg");
+        let selection = svg.selectAll("line").data(this.nodeList)
+        let lines = selection.enter().append("line")
+        selection.exit().remove();
+        selection = lines.merge(selection);
+        selection.attr("x1", (d)=> {return (d.level+1)*150})
+                 .attr("y1", (d)=> {return (d.position+1)*100})
+                 .attr("x2", (d)=> {return d.parentNode ? (d.parentNode.level+1)*150 : (d.level+1)*150;})
+                 .attr("y2", (d)=> {return d.parentNode ? (d.parentNode.position+1)*100 : (d.position+1)*100;});
+
+
+        let nodes = svg.selectAll("nodeGroup").data(this.nodeList)
+        let group = nodes.enter().append("g").classed("nodeGroup", true)
+        nodes.exit().remove();
+        nodes = group.merge(nodes);
+
+        nodes.append("circle")
+            .attr("r", 40);
+
+        nodes.append("text")
+            .attr("class", "label")
+            .text((d)=> {return d.name});
+
+        nodes.attr("transform", (d)=>{return "translate("+[(d.level+1)*150, (d.position+1)*100]+")";});
     }
-
 }
